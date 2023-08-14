b4bc1584885edba5bd4c89503c3aa0adc28edd3e
coolworld2049 <coolworld2049@gmail.com>
2023-08-15 01:20:59 +0300

    aredis

--- kwork_parser_bot/kwork_parser_bot/lifetime.py
+++ kwork_parser_bot/kwork_parser_bot/lifetime.py
@@ -1,5 +1,4 @@
 from aiogram import Dispatcher
-from aredis_om import Migrator
 
 import loader
 from _logging import configure_logging
@@ -18,8 +17,6 @@ async def startup_bot(dp: Dispatcher) -> None:
     await bot.set_my_commands(get_settings().BOT_COMMANDS)
     init_scheduler(loader.scheduler)
     dp.include_routers(menu.router, kwork.router, scheduler.router, settings.router)
-
-    await Migrator().run()
     await loader.prisma.connect()
 
 

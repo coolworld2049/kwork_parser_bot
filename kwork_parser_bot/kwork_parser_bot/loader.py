4ef9f6aa363e54f776bf2e22b5bc77a69ab042c0
coolworld2049 <coolworld2049@gmail.com>
2023-08-15 00:01:31 +0300

    requirements.txt

--- kwork_parser_bot/kwork_parser_bot/loader.py
+++ kwork_parser_bot/kwork_parser_bot/loader.py
@@ -7,7 +7,6 @@ from prisma import Prisma
 from scheduler.scheduler import Scheduler
 from settings import get_settings
 
-os.environ["REDIS_OM_URL"] = get_settings().redis_url
 redis = Redis.from_url(get_settings().redis_url)
 redis_pool = ConnectionPool.from_url(get_settings().redis_url)
 scheduler = Scheduler(timezone=get_settings().TZ)
